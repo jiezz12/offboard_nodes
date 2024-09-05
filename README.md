ae55ad378ec85dcde7f6512bcbd60356a027ebc5
jiezz12 <a2499420178@163.com>
2024-09-05 17:44:21 +0800

    first commit

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 PX4+ROS学习记录
+
 CSDN网站：https://blog.csdn.net/2301_76165902?spm=1000.2115.3001.5343
 
 src:
 offboard_node0-5为简单飞行代码
+
 offboard_figure为阿木实验室水平绕八项目代码
+
 offboard_bazi,offboard_circle为仿照绕八代码的绕圆代码
+
 circle_uav_0为单飞机螺旋飞行
 more_uav_y.cpp和more_uav_z.cpp为双飞机螺旋飞行
+
 123.cpp为xtdrone多无人机测试代码
+
+
 
 scripts:
 keyboard.py为键盘控制px4
+
 more_control.py为多无人机键盘控制
+
 pix2world.py为坐标系转换(像素->世界)
+
 rtsp.py 为rtsp推视频流 用于yolo检测后的画面推送
+
 其他的代码无大用。
+
+
+
 # offboard_nodes
--- src/apriltag_detection.cpp
+++ src/apriltag_detection.cpp
@@ -2,7 +2,7 @@
 #include <apriltag_ros/AprilTagDetectionArray.h>
 
 bool marker_found = false, flag_move = false, flag_land = false;
-int32_t current_target_id = 0;
+std::vector<int> current_target_id (0);
 float detec_x = 0, detec_y = 0, detec_z = 0;
 
 apriltag_ros::AprilTagDetection marker;
@@ -14,13 +14,19 @@ void apriltag_cb(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg)
 		for(int i = 0; i<count; i++)
 		{
 			marker = msg->detections[i];			
+				if(marker.id == current_target_id)
+			{
+				marker_found = true;
+				detec_x = marker.pose.pose.pose.position.x;
+        		detec_y = marker.pose.pose.pose.position.y;
+        		detec_z = marker.pose.pose.pose.position.z; 
+			}
 		}
-        detec_x = marker.pose.pose.pose.position.x;
-        detec_y = marker.pose.pose.pose.position.y;
-        detec_z = marker.pose.pose.pose.position.z; 
-
 	ROS_INFO("detec_x=%.2f,detec_y=%.2f,detec_z=%.2f",detec_x,detec_y,detec_z);
-	
+	}
+	else
+	{
+		marker_found = false;
 	}
 
 }
@@ -32,8 +38,7 @@ int main(int argc, char *argv[])
 	ros::NodeHandle nh;
 
 //订阅识别信息
-ros::Subscriber yolov5_sub = nh.subscribe<apriltag_ros::AprilTagDetectionArray>("/tag_detections", 10, apriltag_cb);
-//发布yolo标志位
+ros::Subscriber apriltag_sub = nh.subscribe<apriltag_ros::AprilTagDetectionArray>("/tag_detections", 10, apriltag_cb);
 
 	ros::Rate loop_rate(20);
     ROS_INFO("begin");
